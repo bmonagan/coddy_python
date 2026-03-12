28f31bb2d69faaa3222a37c6fbc696a6113c0c6b
bmonagan <bernardmonagan@gmail.com>
2026-03-11 23:35:30 -0400

    first commit

---
+++ external_files.py
@@ -0,0 +1,11 @@
+# TODO: update the import
+from my_class import MyClass
+
+# Test code here
+test_case = input()
+print('Test completed')
+
+==========
+class MyClass:
+    def __init__(self):
+        pass
